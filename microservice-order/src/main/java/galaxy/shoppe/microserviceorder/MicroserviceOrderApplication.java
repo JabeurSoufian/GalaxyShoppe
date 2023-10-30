c60821887737326bf578f2f0af7c0fd2f774aba8
soufienjabeur <soufienjabeur@gmail.com>
2023-10-30 20:23:57 +0100

    adding service discovery

--- microservice-order/src/main/java/galaxy/shoppe/microserviceorder/MicroserviceOrderApplication.java
+++ microservice-order/src/main/java/galaxy/shoppe/microserviceorder/MicroserviceOrderApplication.java
@@ -3,12 +3,14 @@ package galaxy.shoppe.microserviceorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceOrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceOrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceOrderApplication.class, args);
+    }
 
 }
