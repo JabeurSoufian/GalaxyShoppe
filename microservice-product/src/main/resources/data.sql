01d7cea4b48ffec06653390452c99541d5a2ec0e
soufienjabeur <soufienjabeur@gmail.com>
2023-11-05 15:17:07 +0100

    code review and functional tests: OK

--- microservice-product/src/main/resources/data.sql
+++ microservice-product/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO product (name, description, price, images, discountPercentage) VALUES
-('T-shirt Naruto Uzumaki', 'T-shirt avec impression de Naruto Uzumaki', 29.99, 'https://example.com/naruto.jpg',0),
-('T-shirt One Piece', 'T-shirt avec impression de One Piece', 24.99, 'https://example.com/one_piece.jpg',10),
-('T-shirt Beach Anime', 'T-shirt avec thème de plage anime', 19.99, 'https://example.com/beach_anime.jpg',30),
-('T-shirt Dragon Ball Z', 'T-shirt avec impression de Dragon Ball Z', 27.99, 'https://example.com/dragon_ball_z.jpg',20),
-('T-shirt Attack on Titan', 'T-shirt avec impression de Attack on Titan', 22.99, 'https://example.com/attack_on_titan.jpg',0);
+INSERT INTO product (id,name, description, price, image, discount_percentage) VALUES
+(1,'T-shirt Naruto Uzumaki', 'T-shirt avec impression de Naruto Uzumaki', 29.99, 'https://example.com/naruto.jpg', 30),
+(2,'T-shirt One Piece', 'T-shirt avec impression de One Piece', 24.99, 'https://example.com/one_piece.jpg',20),
+(3,'T-shirt Beach Anime', 'T-shirt avec thème de plage anime', 19.99, 'https://example.com/beach_anime.jpg',10),
+(4,'T-shirt Dragon Ball Z', 'T-shirt avec impression de Dragon Ball Z', 27.99, 'https://example.com/dragon_ball_z.jpg',50),
+(5,'T-shirt Attack on Titan', 'T-shirt avec impression de Attack on Titan', 22.99, 'https://example.com/attack_on_titan.jpg',10);
 
